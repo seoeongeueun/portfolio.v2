655119ded02b3d30d52c69ce7ad89788b6733a03
seoeongeueun <erin9901@gmail.com>
2025-03-13 01:52:46 +0900

    style: 카트리지 스타일 반응형으로 세분화

--- app/components/cartridge.tsx
+++ app/components/cartridge.tsx
@@ -13,8 +13,9 @@ export default function Cartridge() {
 					<div />
 					<div />
 					<div className="title-container">
-						<div className="title flex flex-col items-center justify-center text-xxl">
-							<p>PROJECT NAME</p>
+						<div className="title flex flex-col items-center justify-center text-m md:text-xxl font-tenada">
+							<p className="tracking-tighter">Project Title</p>
+							<span className="text-xxs md:text-xs">subtitle</span>
 						</div>
 					</div>
 				</div>
@@ -25,6 +26,10 @@ export default function Cartridge() {
 						<div className="thumbnail"></div>
 					</div>
 					<div className="edge right"></div>
+				</div>
+				<div className="tags text-xxxxs md:text-xxxs w-full px-4 md:px-8 gap-1 md:gap-2 flex flex-wrap items-center justify-end">
+					<div className="tag">1 DEV</div>
+					<div className="tag">SOMETHING</div>
 				</div>
 			</div>
 		</div>
